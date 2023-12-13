3b7a0998af739149b25d7bc95a35cc347b778fcf
h2jinee <wjsgmlwls97@gmail.com>
2023-12-13 23:04:53 +0900

    Initial commit

---
+++ src/main/java/com/h2jinee/myrestfulservice/controller/HelloWorldController.java
@@ -0,0 +1,27 @@
+package com.h2jinee.myrestfulservice.controller;
+
+import com.h2jinee.myrestfulservice.bean.HelloWorldBean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+    // GET
+    // URI - /hello-world
+    // @RequestMapping(method=RequestMethod.GET, path="/hello-world")
+    @GetMapping(path = "/hello-world")
+    public String helloWorld() {
+        return "Hello World";
+    }
+
+    @GetMapping(path = "/hello-world-bean")
+    public HelloWorldBean helloWorldBean() {
+        return new HelloWorldBean("Hello World!");
+    }
+
+    @GetMapping(path = "/hello-world-bean/path-variable/{name}")
+    public HelloWorldBean helloWorldBeanPathVariable(@PathVariable String name) {
+        return new HelloWorldBean(String.format("Hello World, %s", name));
+    }
+}
---
+++ src/test/java/com/h2jinee/myrestfulservice/MyRestfulServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.h2jinee.myrestfulservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyRestfulServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
