ab2e7278592e4be2b2e146860927b6d9872ed5df
HEEJIN JEON <wjsgmlwls97@gmail.com>
2023-12-16 20:18:31 +0900

    프로그래밍으로 제어하는 Filtering - 전체 사용자 조회

--- src/main/java/com/h2jinee/myrestfulservice/controller/AdminUserController.java
+++ src/main/java/com/h2jinee/myrestfulservice/controller/AdminUserController.java
@@ -7,14 +7,14 @@ import com.h2jinee.myrestfulservice.bean.AdminUser;
 import com.h2jinee.myrestfulservice.bean.User;
 import com.h2jinee.myrestfulservice.dao.UserDaoService;
 import com.h2jinee.myrestfulservice.exception.UserNotFoundException;
-import jakarta.validation.Valid;
 import org.springframework.beans.BeanUtils;
-import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.json.MappingJacksonValue;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.net.URI;
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -42,6 +42,30 @@ public class AdminUserController {
         FilterProvider filters = new SimpleFilterProvider().addFilter("UserInfo", filter);
 
         MappingJacksonValue mapping = new MappingJacksonValue(adminUser);
+        mapping.setFilters(filters);
+
+        return mapping;
+    }
+
+    // -> /admin/users
+    @GetMapping("/users")
+    public MappingJacksonValue retrieveAllUsers() {
+        List<User> users = service.findAll();
+
+        ArrayList<AdminUser> adminUsers = new ArrayList<>();
+        AdminUser adminUser = null;
+
+        for (User user : users) {
+            adminUser = new AdminUser();
+            BeanUtils.copyProperties(user, adminUser);
+
+            adminUsers.add(adminUser);
+        }
+
+        SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter.filterOutAllExcept("id", "name", "joinDate", "ssn");
+        FilterProvider filters = new SimpleFilterProvider().addFilter("UserInfo", filter);
+
+        MappingJacksonValue mapping = new MappingJacksonValue(adminUsers);
         mapping.setFilters(filters);
 
         return mapping;
