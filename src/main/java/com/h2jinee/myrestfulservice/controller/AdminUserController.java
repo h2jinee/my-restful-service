4d6b2a53f95d6997d072e56e27720b36318ed00e
h2jinee <wjsgmlwls97@gmail.com>
2023-12-17 20:56:01 +0900

    Version 관리 - Paramter와 Header를 이용한 버전관리

--- src/main/java/com/h2jinee/myrestfulservice/controller/AdminUserController.java
+++ src/main/java/com/h2jinee/myrestfulservice/controller/AdminUserController.java
@@ -28,7 +28,10 @@ public class AdminUserController {
     }
 
     // /admin/v1/users/{id}
-    @GetMapping("/v1/users/{id}")
+//    @GetMapping("/v1/users/{id}")
+//    @GetMapping(value = "/users/{id}", params = "version=1")
+//    @GetMapping(value = "/users/{id}", headers = "X-API-VERSION=1")
+    @GetMapping(value = "/users/{id}", produces = "application/vnd.company.appv1+json")
     public MappingJacksonValue retrieveUser4Admin(@PathVariable int id) {
         User user = service.findOne(id);
 
@@ -49,8 +52,12 @@ public class AdminUserController {
     }
 
     // -> /admin/v2/users/{id}
-    @GetMapping("/v2/users/{id}")
-    public MappingJacksonValue retrieveUser4AdminV2(@PathVariable int id) {
+//    @GetMapping("/v2/users/{id}")
+//    @GetMapping(value = "/users/{id}", params = "version = 2")
+//    @GetMapping(value = "/users/{id}", headers = "X-API-VERSION=2")
+    @GetMapping(value = "/users/{id}", produces = "application/vnd.company.appv2+json")
+    public MappingJacksonValue retrieveUser4AdminV2(@PathVariable int id
+    ) {
         User user = service.findOne(id);
 
         AdminUserV2 adminUser = new AdminUserV2();
