fcbfeb25af6e0eb5d0ba5a65f8a238969fae6d7d
h2jinee <wjsgmlwls97@gmail.com>
2023-12-21 21:34:17 +0900

    JPA를 이용한 개별 사용자 상세 조회 - HTTP Get method

--- src/main/java/com/h2jinee/myrestfulservice/controller/UserJpaController.java
+++ src/main/java/com/h2jinee/myrestfulservice/controller/UserJpaController.java
@@ -1,12 +1,21 @@
 package com.h2jinee.myrestfulservice.controller;
 
 import com.h2jinee.myrestfulservice.bean.User;
+import com.h2jinee.myrestfulservice.exception.UserNotFoundException;
 import com.h2jinee.myrestfulservice.repository.UserRepository;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 @RequestMapping("/jpa")
@@ -22,5 +31,22 @@ public class UserJpaController {
     @GetMapping("/users")
     public List<User> retrieveAllUsers() {
         return userRepository.findAll();
+    }
+
+    // /jpa/users/{id}
+    @GetMapping("/users/{id}")
+    public ResponseEntity retrieveUserById(@PathVariable int id) {
+        Optional<User> user = userRepository.findById(id);
+
+        if (user.isEmpty()) {
+            throw new UserNotFoundException("id - " + id);
+        }
+
+        EntityModel<User> entityModel = EntityModel.of(user.get());
+
+        WebMvcLinkBuilder linTo = linkTo(methodOn(this.getClass()).retrieveAllUsers());
+        entityModel.add(linTo.withRel("all-users")); // all-users -> http://localhost:8088/users
+
+        return ResponseEntity.ok(entityModel);
     }
 }
