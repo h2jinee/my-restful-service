443c94fc7b4a1328f314f382d3b63cbd58d26369
HEEJIN JEON <wjsgmlwls97@gmail.com>
2023-12-23 19:30:19 +0900

    게시물 관리를 위한 Post Entity 추가와 초기 데이터 생성

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,7 @@
 insert into users(id, join_date, name, password, ssn) values (90001, now(), 'User1', 'test111', '101010-1111111');
 insert into users(id, join_date, name, password, ssn) values (90002, now(), 'User2', 'test222', '222222-1111111');
 insert into users(id, join_date, name, password, ssn) values (90003, now(), 'User3', 'test333', '333333-1111111');
+
+
+insert into post(description, user_id) values ('My first Post', 90001);
+insert into post(description, user_id) values ('My second Post', 90001);
